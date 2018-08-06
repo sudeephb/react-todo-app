d859560e196343d9641d6f93571853ebba748d66
sudeephb <sudeephb19@gmail.com>
2018-08-06 08:30:30 +0545

    3/4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import AddTodo from './components/addTodo/';
+import actions from './actions';
 
-const App = () => (
+const App = ({submitTodo}) => (
     <div>
     <h1>Todo List</h1>
-    <AddTodo submitTodo={() => {}} />
+    <AddTodo submitTodo={submitTodo} />
     </div>
 );
 
-export default App;
+
+App.PropTypes = {
+    submitTodo: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state.todoListApp;
+
+const mapDispatchToProps = dispatch => ({
+    submitTodo: (text) => {
+        if(text){
+            dispatch(actions.submitTodo(text));
+        }
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,21 @@
 /* global it, expect */
+
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import {App} from './App';
+import { initialState } from './reducers/';
 
 configure({adapter: new Adapter()});
 
 it('renders without crashing', () => {
-  const component = shallow(<App />);
+  const mockFunction = jest.fn();
+
+  const component = shallow(<App
+                              state = {initialState}
+                              submitTodo = {mockFunction}
+                               />,
+                              );
+
   expect(component.exists()).toEqual(true);
 });
